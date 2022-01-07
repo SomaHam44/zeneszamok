4f7d6346f23bfd2ae0279af359843934e8eec0f7
SomaHam44 <somasolti@gmail.com>
2022-01-07 14:52:47 +0100

    first commit

---
+++ zeneszamok/resources/views/main.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Songs</h1>
+    <table>
+        <tr>
+            <th>Title</th>
+            <th>Singer</th>
+            <th>Length (sec)</th>
+        </tr>
+        @foreach ($songs->sortBy('title') as $song)
+            <tr>
+                <td>{{$song->title}}</td>
+                <td>{{$song->singer}}</td>
+                <td>{{$song->length}} s</td>
+            </tr>
+        @endforeach
+
+
+
+    </table>
+</body>
+</html>
